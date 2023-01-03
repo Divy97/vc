a9d44d85f11174a9ff6711c8b6d173fc62168223
Divy <divyparekh1810@gmail.com>
2023-01-03 21:55:30 +0530

    Live chat added

--- public/js/room_rtc.js
+++ public/js/room_rtc.js
@@ -38,6 +38,7 @@ let joinRoomInit = async () => {
 
   channel.on("MemberJoined", handleMemberJoined);
   channel.on("MemberLeft", handleMemberLeft);
+  channel.on("ChannelMessage", handleChannelMessage);
 
   getMembers();
 
