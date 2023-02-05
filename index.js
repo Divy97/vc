f6b968dd7f12b6949523160a9a9074b214485d1f
Divy <divyparekh1810@gmail.com>
2023-02-05 20:32:10 +0530

    connected mongo with the website, fixed some bugs

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 const { RtcTokenBuilder, RtcRole } = require("agora-access-token");
-
-const PORT = 8000;
+// const { userRoutes } = require("./Routes/userRoutes");
+const dotenv = require("dotenv");
 const app = express();
+
+const connectDB = require("./config/db");
+dotenv.config();
+connectDB();
 
 const APP_ID = "bfca05642cd54e18bac76fe16727eb02";
 const APP_CERTIFICATE = "996c157f17ea44bdb5a792b93566c767";
 let TOKEN = null;
 let CHANNEL = null;
+
+const PORT = process.env.PORT || 9000;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -27,12 +33,13 @@ app.get("/room.ejs", (req, res) => {
   res.render("room.ejs");
 });
 
+// app.use("/room", userRoutes);
+
 app.get("/:roomId", function (req, res) {
   res.render("room.ejs", {
     APP_ID: APP_ID,
     CHANNEL: req.params.roomId,
     TOKEN: TOKEN,
-    USERNAME: USERNAME,
   });
 });
 
