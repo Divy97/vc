b469ce735898cc6f20466d7f3bb2855594b77f17
Divy <divyparekh1810@gmail.com>
2023-02-05 23:26:46 +0530

    created user and deployed it to mongodb

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -1,9 +1,32 @@
-const registerUser = async (req, res) => {
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
-  res.json({
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = User.create({
     name,
     email,
+    password,
   });
-};
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      isAdmin: user.isAdmin,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Error Occurred while creating user!");
+  }
+});
 
 module.exports = { registerUser };
