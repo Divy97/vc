7171e25fa2f4138cfae8dc5bf30e1d00fee8190e
Divy <divyparekh1810@gmail.com>
2023-02-12 23:45:28 +0530

    Login page and loader done

--- Controllers/userControllers.js
+++ Controllers/userControllers.js
@@ -35,7 +35,7 @@ const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
-  if (user && (await user.n(password))) {
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,51 @@
 // const axios = require("axios");
-let login_userEmail = document.getElementById("email").value;
-let login_userPassword = document.getElementById("password").value;
-let loading = false;
-let error = "";
+const userInfo = localStorage.getItem("userInfo");
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  console.log(login_userEmail);
-  // try {
-  //   const config = {
-  //     headers: {
-  //       "Content-type": "application/json",
-  //     },
-  //   };
-  //   loading = true;
-  //   const { data } = await axios.post(
-  //     "/login",
-  //     {
-  //       login_userEmail,
-  //       login_userPassword,
-  //     },
-  //     config
-  //   );
-  //   console.log(data);
-  //   localStorage.setItem("user-info", JSON.stringify(data));
-  //   loading = false;
-  // } catch (error) {
-  //   error = error.response.data.message;
-  // }
-};
+if (userInfo) {
+  location.href = "http://localhost:8000/room";
+} else {
+  let loginForm = document.getElementById("loginForm");
+  const loader = document.getElementById("loading");
+  loader.classList.add("loading-hidden");
+  let error = "";
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    loader.classList.remove("loading-hidden");
+
+    let login_userEmail = document.getElementById("email").value;
+    let login_userPassword = document.getElementById("password").value;
+    console.log(login_userEmail, login_userPassword);
+    try {
+      let raw = JSON.stringify({
+        email: login_userEmail,
+        password: login_userPassword,
+      });
+      let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      let data = await fetch("http://localhost:8000/room/login", requestOptions)
+        .then((response) => response.text())
+        .then((result) => result)
+        .catch((error) => console.log("error", error));
+
+      console.log("data" + data);
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      loader.classList.add("loading-hidden");
+      location.href = "http://localhost:8000/room";
+      login_userEmail = document.getElementById("email").value = "";
+      login_userEmail = document.getElementById("password").value = "";
+    } catch (error) {
+      error = error.response.data.message;
+    }
+  };
+
+  loginForm.addEventListener("submit", handleSubmit);
+}
